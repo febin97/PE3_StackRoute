a29c1bc9d3763b0ec5b4c4a0d66e24b0b715d5cd
febin.thomas <febin.thomas@cgi.com>
2019-09-12 11:39:48 +0530

    Test Cases

--- src/main/java/com/stackroute/Matrix.java
+++ src/main/java/com/stackroute/Matrix.java
@@ -3,50 +3,10 @@ package com.stackroute;
 import java.util.Arrays;
 
 public class Matrix {
-    int noOfRows;
-    int noOfColumns;
-    int[][] elements;
 
-    public Matrix(int noOfRows, int noOfColumns, int[][] elements) {
-        this.noOfRows = noOfRows;
-        this.noOfColumns = noOfColumns;
-        this.elements = elements;
-    }
-
-    public Matrix() {
-
-    }
-
-    @Override
-    public String toString() {
-        return null;
-    }
 
     public Matrix matrixAdd(Matrix B){
         return null;
     }
-
-    public int[][] getElements() {
-        return elements;
-    }
-
-    public void setElements(int[][] elements) {
-        this.elements = elements;
-    }
-
-    public int getNoOfRows() {
-        return noOfRows;
-    }
-
-    public void setNoOfRows(int noOfRows) {
-        this.noOfRows = noOfRows;
-    }
-
-    public int getNoOfColumns() {
-        return noOfColumns;
-    }
-
-    public void setNoOfColumns(int noOfColumns) {
-        this.noOfColumns = noOfColumns;
-    }
+    
 }
--- src/main/java/com/stackroute/Places.java
+++ src/main/java/com/stackroute/Places.java
@@ -2,24 +2,6 @@ package com.stackroute;
 
 public class Places {
     public String[] placesWithoutVowels(String[] strArr){
-        if(strArr==null){
-            return null;
-        }
-        StringBuilder sb;
-        for (int i=0;i<strArr.length;i++){
-            String strCurr = strArr[i];
-            sb = new StringBuilder(strCurr);
-            int j=0;
-            while (j<sb.length()){
-                char ch = sb.charAt(j);
-                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-                    sb.deleteCharAt(j);
-                    continue;
-                }
-                j++;
-            }
-            strArr[i] = sb.toString();
-        }
-        return strArr;
+        return null;
     }
 }
