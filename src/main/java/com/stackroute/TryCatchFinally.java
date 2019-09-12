bf67e614781a333248d1465ed75a471ae2d2d0bb
febin.thomas <febin.thomas@cgi.com>
2019-09-12 12:28:53 +0530

    7th Qn

---
+++ src/main/java/com/stackroute/TryCatchFinally.java
@@ -0,0 +1,7 @@
+package com.stackroute;
+
+public class TryCatchFinally {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ src/test/java/com/stackroute/TryCatchFinallyTest.java
@@ -0,0 +1,31 @@
+package com.stackroute;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class TryCatchFinallyTest {
+    TryCatchFinally T;
+    @Before
+    public void setUp() throws Exception {
+        T  = new TryCatchFinally();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        T=null;
+    }
+
+    @Test
+    public void main_PrintErrorMsgInCatchBlockGoToFinally() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        String testStr = "Error Caught in Catch Block\n"+"Now in Finally Block\n";
+        assertEquals("check your main",testStr,output.toString());
+    }
+}
